0e6caf6f69580c1ee25e7730cd5e1d70792bad72
Shambhavi Dubey <shambhavidubey3@gmail.com>
2019-09-11 00:50:00 +0530

    Initial commit

---
+++ script3.js
@@ -0,0 +1,34 @@
+var clock = document.getElementById('clock');
+var hexColor = document.getElementById('hex-color');
+
+function hexClock(){
+	var time = new Date();
+	var hours = (time.getHours() % 12).toString();
+	var minutes = time.getMinutes().toString();
+	var seconds = time.getSeconds().toString();
+
+	if(hours.length < 2){
+		hours = '0' + hours;
+	}
+
+	if(minutes.length < 2){
+		hours = '0' + minutes;
+	}
+
+	if(seconds.length < 2){
+		hours = '0' + seconds;
+	}
+
+	var clockStr = hours + ':' + minutes + '.' + seconds;
+
+	var hexColorStr = '#' + hours + minutes + seconds;
+
+    clock.textContent = clockStr;
+	hexColor.textContent = hexColorStr;
+
+	document.body.style.backgroundColor = hexColorStr;
+}
+
+hexClock();
+
+setInterval(hexClock, 1000);    ;
